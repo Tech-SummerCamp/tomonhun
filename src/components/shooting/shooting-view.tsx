24d83a6c8212bb6bd6e36c1988430f925f978103
Yuto Terada <lugongdao07@gmail.com>
2024-09-15 22:30:31 +0900

    討伐終了ボタン

--- src/components/shooting/shooting-view.tsx
+++ src/components/shooting/shooting-view.tsx
@@ -126,6 +126,7 @@ export function ShootingView({ avatars }: { avatars: Avatar[] }) {
 
     const camera = new Three.PerspectiveCamera(45, w / h, 1, 10000);
     camera.position.set(0, 0, 1);
+    camera.position.set(0, 0, 1);
     cameraRef.current = camera;
     if (/Mobi|Android/i.test(navigator.userAgent)) {
       console.log('DeviceOrientation is supported');
@@ -303,7 +304,7 @@ export function ShootingView({ avatars }: { avatars: Avatar[] }) {
         className='fixed left-0 top-0 bg-transparent'
       />
       <StockView avatars={stock} />
-      <Button className='bottom fixed bottom-28 left-4 z-10 h-28 w-28 text-wrap rounded-full bg-red-400 text-lg hover:bg-red-500 md:bottom-4'>
+      <Button className='z-10 fixed bottom w-28 h-28 text-lg text-wrap rounded-full bg-red-400 hover:bg-red-500 left-4 bottom-28 md:bottom-4'>
         討伐を終了する
       </Button>
 
