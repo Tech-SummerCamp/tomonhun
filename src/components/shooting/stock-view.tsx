211f33b70dbd0e4a1ed456853edd4f68d7ff7b55
Yuto Terada <lugongdao07@gmail.com>
2024-09-16 00:06:42 +0900

    リファクタリング

--- src/components/shooting/stock-view.tsx
+++ src/components/shooting/stock-view.tsx
@@ -2,20 +2,19 @@ import type { Avatar } from '@/lib/shooting/avatar';
 
 export function StockView({ avatars }: { avatars: Avatar[] }) {
   return (
-    <div className='fixed bottom-4 flex w-full justify-center gap-4'>
+    <div className='flex gap-4 w-full bottom-4 justify-center fixed'>
       {avatars.map((item, index) => (
         <div
           key={index}
           className='flex-[1/5] bg-[#D9D9D9] border-[#F5F5F5] border-4 w-20 max-h-20 aspect-square'
         >
           <img src={item.imageUrl} alt='avatar' className='aspect-auto' />
-          {/* <p>message</p> */}
         </div>
       ))}
       {Array.from({ length: 5 - avatars.length }).map((_, index) => (
         <div
           key={index}
-          className='aspect-square max-h-20 w-20 border-4 border-[#F5F5F5] bg-[#D9D9D9]'
+          className='bg-[#D9D9D9] border-[#F5F5F5] border-4 w-20 max-h-20 aspect-square'
         />
       ))}
     </div>
