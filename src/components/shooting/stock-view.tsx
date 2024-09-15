a48e7e896702988f70fc713a40c5ebd0fe4e62b6
Yuto Terada <lugongdao07@gmail.com>
2024-09-16 00:05:23 +0900

    stock-viewのレスポンシブ改善

--- src/components/shooting/stock-view.tsx
+++ src/components/shooting/stock-view.tsx
@@ -6,9 +6,10 @@ export function StockView({ avatars }: { avatars: Avatar[] }) {
       {avatars.map((item, index) => (
         <div
           key={index}
-          className='aspect-square max-h-20 w-20 flex-[1/5] border-4 border-[#F5F5F5] bg-[#D9D9D9]'
+          className='flex-[1/5] bg-[#D9D9D9] border-[#F5F5F5] border-4 w-20 max-h-20 aspect-square'
         >
           <img src={item.imageUrl} alt='avatar' className='aspect-auto' />
+          {/* <p>message</p> */}
         </div>
       ))}
       {Array.from({ length: 5 - avatars.length }).map((_, index) => (
