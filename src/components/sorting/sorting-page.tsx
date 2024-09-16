dae07e6bed0bf1958ceb0254a8933d7485c42ec2
Yuto Terada <lugongdao07@gmail.com>
2024-09-16 11:41:42 +0900

    sorting-pageでlocalstorageから読み込むようにした

--- src/components/sorting/sorting-page.tsx
+++ src/components/sorting/sorting-page.tsx
@@ -2,21 +2,33 @@
 
 import { Avatar } from '@/lib/shooting/avatar';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SerifScreen } from './serif-screen';
 import { SortingScreen } from './sorting-screen';
 
 export default function SortingClientPage({
-  avatars,
   initialRemainCount,
 }: {
-  avatars: Avatar[];
   initialRemainCount: number;
 }) {
+  const [avatars, setAvatars] = useState<Avatar[]>([]);
   const [index, setIndex] = useState(0);
   const [remain, setRemain] = useState(initialRemainCount);
   const [serifScreen, setSerifScreen] = useState(true);
   const router = useRouter();
+
+  useEffect(() => {
+    const str = localStorage.getItem('avatars');
+    if (str) {
+      const obj = JSON.parse(str) as Avatar[];
+      if (obj) {
+        setAvatars(obj);
+        return;
+      }
+    }
+
+    router.replace('/zukan');
+  }, [router]);
 
   const transition = (serifScreen: boolean, increment: boolean) => {
     if (increment) {
@@ -30,10 +42,12 @@ export default function SortingClientPage({
   };
 
   const handleReleaseButtonClick = () => {
+    const i = index;
+    setAvatars((avatars) => avatars.slice(i, 1));
     if (index === avatars.length - 1) {
       router.replace('/zukan');
     } else {
-      transition(true, true);
+      transition(true, false);
     }
   };
 
