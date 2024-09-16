a29f48135e001c7e200e40551599e360ccc2e217
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-09-16 09:59:27 +0900

    chore: Add Twitter as a provider for NextAuth authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
+import Twitter from 'next-auth/providers/twitter';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Twitter],
 });
